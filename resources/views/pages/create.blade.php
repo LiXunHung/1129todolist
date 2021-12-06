e993a650c4e1bb72a439a88f96e05326a87df006
lixun <40841141@gm.nfu.edu.tw>
2021-12-06 18:17:56 +0800

    新增create.blade,更改index.blade的內容

---
+++ resources/views/pages/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+@section('content')
+    <form>
+        <label>標題</label>
+        <input name="title">
+        <label>內容</label>
+        <input name="content">
+        <label>備註</label>
+        <input name="remark">
+        <button type="submit">submit</button>
+    </form>
+@endsection
