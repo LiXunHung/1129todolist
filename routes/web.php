ba43dead492dd2ae07e763c07e0096f5e030ed2a
lixun <40841141@gm.nfu.edu.tw>
2021-12-27 18:17:00 +0800

    增加修改刪除路徑

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,11 @@ use Illuminate\Support\Facades\Route;
 //    return view('layout.master');
 //});
 Route::get('/',[\App\Http\Controllers\TodoController::class,'index'])->name('index');
+//新增
 Route::get('/create',[\App\Http\Controllers\TodoController::class,'get_create_data'])->name('get_create_data');
-Route::post('/create',[\App\Http\Controllers\TodoController::class,'store_create'])->name('store_create');
+Route::post('/create',[\App\Http\Controllers\TodoController::class,'store_create'])->name('store_create_data');
+//修改
+Route::get('/edit',[\App\Http\Controllers\TodoController::class,'get_edit_data'])->name('get_edit_data');
+Route::post('/edit',[\App\Http\Controllers\TodoController::class,'store_edit'])->name('store_edit_data');
+//刪除
+Route::get('/delete',[\App\Http\Controllers\TodoController::class,'delete_data'])->name('delete_data');
