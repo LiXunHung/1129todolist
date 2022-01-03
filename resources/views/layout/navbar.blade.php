eba98f9f459774a452e9142cf8d2eb243b37aa83
LiXun <40841141@gm.nfu.edu.tw>
2022-01-03 16:24:19 +0800

    更改畫面樣式

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -7,12 +7,11 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{route('get_create_data')}}">新增備忘錄</a>
+                <a class="nav-link" href="{{route('get_create_page')}}">新增備忘錄</a>
             </li>
-
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <form class="form-inline my-2 my-lg-0" action="{{route('index')}}">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search_content">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
