0995e5a9dd10e6b9b0f24a767696d3eda2a411fd
LiXun <40841141@gm.nfu.edu.tw>
2022-01-03 16:24:45 +0800

    新增訊息提示

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,13 +1,31 @@
 @extends('layout.master')
 @section('content')
-    <form action="{{route('store_create')}}" method="post">
-        @csrf
-        <label>標題</label>
-        <input name="title">
-        <label>內容</label>
-        <input name="content">
-        <label>備註</label>
-        <input name="remark">
-        <button type="submit">submit</button>
-    </form>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <div class="container">
+        <form class="form-group m-5" action="{{route('store_create_data')}}" method="post">
+            @csrf
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">標題</label>
+                <input class="form-control col-sm-6" name="title">
+            </div>
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">內容</label>
+                <input class="form-control col-sm-6" name="content">
+            </div>
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">備註</label>
+                <input class="form-control col-sm-6" name="remark">
+            </div>
+            <div class="row form-group justify-content-center">
+                <script>
+                    function time_delay() {
+                        swal("Good job!", "You clicked the button!", "success")
+                    }
+                </script>
+                <button class="btn btn-outline-info col-sm-3" onclick="time_delay()" >送出新增</button>
+            </div>
+        </form>
+    </div>
+
+
 @endsection
