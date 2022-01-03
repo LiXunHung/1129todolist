17418f5d4ca80656cf728d4840ca5d13a43e6479
LiXun <40841141@gm.nfu.edu.tw>
2022-01-03 16:23:10 +0800

    新增搜尋功能

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,66 +7,70 @@ use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $data = DB::table('todos')->get();
-        //dd($data,'123');
+        $search_content = $request->get('search_content');
+        $data = DB::table('todos');
+        if ($search_content) {
+            $data->orwhere('title','like','%'. $search_content .'%');
+            $data->orwhere('content','like','%'. $search_content .'%');
+            $data->orwhere('remark','like','%'. $search_content .'%');
+        }
+        $data = $data->get();
+//        dd($data);
         return view('pages.index',compact('data',$data));
-
     }
 
-    public function get_create_data()
-    {
+    public function get_create_page(){
         return view('pages.create');
     }
 
-    public function store_create(Request $request)
+    public function store_create_data (Request $request)
     {
-        $title = $request->get("title");
-        $content = $request->get("content");
-        $remark = $request->get("remark");
+        $title = $request->get('title');
+        $content = $request->get('content');
+        $remark = $request->get('remark');
 
-        DB::table("todos")->insert([
-            "title" => $title,
-            "content" => $content,
-            "remark" =>$remark
+        DB::table('todos')->insert([
+            'title' => $title,
+            'content' => $content,
+            'remark' => $remark,
         ]);
-        return view('pages.create');
-
+        return redirect()->route('index');
     }
 
     public function delete_data(Request $request)
     {
-        $id = $request ->delete_id;
+        $id = $request->get('id');
         DB::table('todos')
-            ->where('id',$id)
+            ->where('id', $id)
             ->delete();
         return redirect()->route('index');
     }
 
-    public function get_edit_data(Request $request)
-    {
-        $id = $request ->edit_id;
-        $data=DB::table('todos')
-            ->where('id',$id)
-            ->first();
-        return view('pages.edit',compact('data'));
-    }
-
-    public function store_edit(Request $request)
+    public function get_edit_page(Request $request)
     {
         $id = $request->get('id');
-        $title = $request->get("title");
-        $content = $request->get("content");
-        $remark = $request->get("remark");
+        $data = DB::table('todos')
+            ->where('id', $id)
+            ->first();
+        return view('pages.edit',compact('data',$data));
+    }
 
-        DB::table("todos")
-            ->where('id',$id)
+    public function store_edit_data(Request $request)
+    {
+        $id = $request->get('id');
+        $title = $request->get('title');
+        $content = $request->get('content');
+        $remark = $request->get('remark');
+
+        $test = DB::table('todos')
+            ->where('id', $id)
             ->update([
-            "title" => $title,
-            "content" => $content,
-            "remark" =>$remark
-        ]);
+                'title' => $title,
+                'content' => $content,
+                'remark' => $remark,
+            ]);
         return redirect()->route('index');
     }
 }
