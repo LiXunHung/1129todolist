0ff91e92b605977b4fec3186e0b9dbf3ee817b88
LiXun <40841141@gm.nfu.edu.tw>
2022-01-03 16:25:03 +0800

    更改畫面樣式新增提示

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,13 +1,28 @@
 @extends('layout.master')
 @section('content')
-    <form action="{{route('get_edit_data')}}" method="post">
-        @csrf
-        <label>標題</label>
-        <input name="title" value="{{$data->title}}">
-        <label>內容</label>
-        <input name="content" value="{{$data->content}}">
-        <label>備註</label>
-        <input name="remark"value="{{$data->remark}}">
-        <button type="submit">submit</button>
-    </form>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <div class="container">
+        <form class="form-group m-5" action="{{route('store_edit_data')}}" method="post">
+            @csrf
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">標題</label>
+                <input class="form-control col-sm-6" name="title" value="{{$data->title}}">
+            </div>
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">內容</label>
+                <input class="form-control col-sm-6" name="content" value="{{$data->content}}">
+            </div>
+            <div class="row form-group">
+                <label class="col-sm-3 text-right">備註</label>
+                <input class="form-control col-sm-6" name="remark" value="{{$data->remark}}">
+            </div>
+            <div class="row form-group justify-content-center">
+                <button class="btn btn-outline-info col-sm-3" onclick="swal('Good job!', '修改成功!', 'success'); " type="submit">送出儲存</button>
+            </div>
+            <input type="hidden" name="id" value="{{$data->id}}"/>
+        </form>
+    </div>
+
+
 @endsection
